aaaafca0c9670afe4cc93d1d886f30fac33fd807
Atesam Abdullah <atesamabdullah8@gmail.com>
2023-12-08 17:24:13 +0500

    runs till test7

--- wish.c
+++ wish.c
@@ -1,104 +1,171 @@
-#include "wish.h"   // printError, parseInput, searchPath, redirect, executeCommands, trim, clean
-#include <ctype.h>  // isspace
-#include <regex.h>  // regcomp, regexec, regfree
-#include <stdio.h>  // fopen, fclose, fileno, getline, feof
-#include <stdlib.h> // exit
-#include <sys/types.h>
-#include <sys/wait.h> // waitpid
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-// Function to parse and run UNIX commands
-int execute(char *buffer)
-{
-    char *path = "/bin/"; // path to bash
-    char *args[50];       // args in command
-    char *token;          // one word
-    int i = 0;
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 64
+#define MAX_PATHS 64
 
-    // split the command on whitespace
-    while ((token = strsep(&buffer, " \t\n\v\f\r")) != NULL)
-    {
-        if (strlen(token) > 0) // if token not null
-        {
-            // printf("(%s), len:%zu\n", token, strlen(token));
-            args[i++] = token;
+char* paths[MAX_PATHS]; // Array to store paths
+int numPaths = 0; // Number of paths in the array
+
+// Function to print error message
+void printError() {
+    char error_message[30] = "An error has occurred\n";
+    write(STDERR_FILENO, error_message, strlen(error_message)); 
+}
+
+// Function to execute command or shell script
+void executeCommand(char **args, int background) {
+    int i;
+    pid_t pid;
+    int found_executable = 0;
+    char cmd[MAX_INPUT_SIZE];
+
+    // Try to find the shell script in each path in the paths array
+    for (i = 0; i < numPaths; i++) {
+        snprintf(cmd, sizeof(cmd), "%s/%s.sh", paths[i], args[0]);
+        if (access(cmd, X_OK) == 0) {
+            found_executable = 1;
+            break;
         }
     }
-    args[i] = NULL; // Null-terminate the argument array
 
-    printf("%s,%zu,%s,%zu", path, strlen(path), args[0], strlen(args[0]));
-    // strcat(path, args[0]); // causes seg fault
-
-    // create child process
-    pid_t pid = fork();
-
-    if (pid == -1) // fail
-    {
-        printf("Fork Failed");
-        return 1;
+    if (!found_executable) {
+        // If .sh file not found, try to find executable command
+        for (i = 0; i < numPaths; i++) {
+            snprintf(cmd, sizeof(cmd), "%s/%s", paths[i], args[0]);
+            if (access(cmd, X_OK) == 0) {
+                found_executable = 1;
+                break;
+            }
+        }
     }
-    else if (pid == 0) // child
-    {
-        execv(path, args); // add command into path e.g: ls ---> /usr/bin/ls
-        perror("execv");
+
+    if (!found_executable) {
+        printError();
+        return;
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        printError();
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process
+        execv(cmd, args);
+        // If execv returns, it must have failed
+        printError();
+        exit(EXIT_FAILURE);
+    } else {
+        // Parent process
+        if (!background) {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+}
+
+// Function to handle built-in commands
+int handleBuiltInCommands(char **args) {
+    if (strcmp(args[0], "exit") == 0) {
+        if (args[1] != NULL) {
+            printError();
+        }
+        exit(EXIT_SUCCESS);
+    } else if (strcmp(args[0], "cd") == 0) {
+        if (args[1] == NULL) {
+            printError();
+        } else {
+            if (chdir(args[1]) != 0) {
+                printError();
+            }
+        }
+        return 1;
+    } else if (strcmp(args[0], "path") == 0) {
+        // Clear existing paths
+        // numPaths = 0;
+
+        // Skip the "path" command itself and update paths with provided directories
+        for (int i = 1; args[i] != NULL; i++) {
+            paths[numPaths++] = args[i];
+        }
+        // printf("paths %d\n",numPaths);
+
+        return 1;
     }
     return 0;
 }
 
-int main(int argc, char *argv[])
-{
-    char *buffer = NULL;
-    size_t len = 0;
-    int line;
+// Function to parse input command
+void parseInput(char *input) {
+    char *token;
+    char *args[MAX_ARGS];
+    int background = 0;
 
-    //  BATCH MODE
-    if (argc > 1)
-    {
-        FILE *file; // file ptr
+    token = strtok(input, " \t\n");
+    int i = 0;
+    while (token != NULL) {
+        if (strcmp(token, "&") == 0) {
+            background = 1;
+            break;
+        }
+        args[i++] = token;
+        // printf("arg[%d] = %s\n",i-1,args[i-1]);
+        token = strtok(NULL, " \t\n");
+    }
+    args[i] = NULL;
 
-        if ((file = fopen(argv[1], "r")) == NULL) // open file
-        {
-            fprintf(stderr, "Input file (%s) not found.\n", argv[1]); // !!! file cannot be opened
-            exit(1);
+    if (args[0] == NULL) {
+        return;
+    }
+
+    if (!handleBuiltInCommands(args)) {
+        // Not a built-in command
+        executeCommand(args, background);
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    char input[MAX_INPUT_SIZE];
+    FILE *file = NULL;
+
+    // Check if batch mode
+    if (argc > 1) {
+        file = fopen(argv[1], "r");
+        if (file == NULL) {
+            printError();
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    numPaths++;
+    paths[0] = "/bin"; // Initial directory
+
+    while (1) {
+        if (file == NULL) {
+            printf("wish> ");
+            fflush(stdout);
+            if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+                break;
+            } 
+        } else {
+            if (fgets(input, MAX_INPUT_SIZE, file) == NULL) {
+                break;
+            }
         }
 
-        while ((line = getline(&buffer, &len, file)) != -1) // read file linewise
-        {
-            if (buffer[line - 1] == '\n') // Remove the trailing newline character
-                buffer[line - 1] = '\0';
+        input[strcspn(input, "\n")] = '\0'; // Remove newline
 
-            // printf("%s\n", buffer);
-        }
+        parseInput(input);
+    }
 
+    if (file != NULL) {
         fclose(file);
-        free(buffer);
-        exit(0); // EOF
     }
 
-    // INTERACTIVE MODE
-    while (1)
-    {
-        printf("wish> ");
-
-        // read line
-        line = getline(&buffer, &len, stdin);
-
-        // no comand entered
-        if (line == -1)
-            continue;
-
-        // Remove the trailing newline character
-        if (buffer[line - 1] == '\n')
-            buffer[line - 1] = '\0';
-
-        // break loop on "exit"
-        if (strcasecmp(buffer, "exit") == 0)
-        {
-            printf("EXIT");
-            free(buffer);
-            exit(0); // EOF
-        }
-        // printf("%s\n", buffer);
-        // execute(buffer);
-    }
     return 0;
 }
--- wish.h
+++ wish.h
@@ -4,37 +4,32 @@
 #include <string.h>  // strlen, strsep, strcat, strdup, strcmp
 #include <unistd.h>  // STDERR_FILENO, fork, exec, access, exit, chdir
 
-#ifdef REG_ENHANCED // macOS: man re_format
-#define REG_CFLAGS REG_EXTENDED | REG_NOSUB | REG_ENHANCED
+#ifdef REG_ENHANCED  // macOS: man re_format
+#  define REG_CFLAGS REG_EXTENDED | REG_NOSUB | REG_ENHANCED
 #else
-#define REG_CFLAGS REG_EXTENDED | REG_NOSUB
+#  define REG_CFLAGS REG_EXTENDED | REG_NOSUB
 #endif
 
 #define INTERACTIVE_MODE 1
 #define BATCH_MODE 2
 #define BUFF_SIZE 256
-#define MAX_ARGS 50
 
-struct function_args
-{
+struct function_args {
   pthread_t thread;
   char *command;
 };
 
-// Standard error message
-void printError()
-{
+void printError() {
   char error_message[30] = "An error has occurred\n";
   write(STDERR_FILENO, error_message, strlen(error_message));
 }
 
-void print_error(char *msg);
-void parse_line(char *line, char **arg);
-void execute_path(char **arg);
-void execute_cd(char **arg);
-void execute_cmd(char **arg);
-void reduce_arg(char **arg);
-void del(char **arg);
-int is_built_int(char *cmd);
-int check_redirection(char **arg);
+void *parseInput(void *arg);
 
+int searchPath(char path[], char *firstArg);
+
+void redirect(FILE *out);
+
+void executeCommands(char *args[], int args_num, FILE *out);
+
+char *trim(char *);
